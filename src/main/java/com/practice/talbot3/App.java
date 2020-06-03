d0a399d9940c098e7f3844c22ebfccca99eda656
wangcheng <wangcheng@deepblueai.com>
2020-06-03 19:32:41 +0800

    feat: rxjava play

--- src/main/java/com/practice/talbot3/App.java
+++ src/main/java/com/practice/talbot3/App.java
@@ -3,57 +3,20 @@
  */
 package com.practice.talbot3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Comparator;
-import lib_talbot3.*;
+import io.reactivex.rxjava3.core.*;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class App {
    public String getGreeting() {
       return "hello world";
    }
-    public static void main(String args[]){
-        Library  lb = new Library();
-        lb.someLibraryMethod();
-        System.out.println(lb.someLibraryMethod());
-
-        List<String> names1 = new ArrayList<String>();
-        names1.add("Google ");
-        names1.add("Runoob ");
-        names1.add("Taobao ");
-        names1.add("Baidu ");
-        names1.add("Sina ");
-          
-        List<String> names2 = new ArrayList<String>();
-        names2.add("Google ");
-        names2.add("Runoob ");
-        names2.add("Taobao ");
-        names2.add("Baidu ");
-        names2.add("Sina ");
-          
-        App tester = new App();
-        System.out.println("使用 Java 7 语法: ");
-          
-        tester.sortUsingJava7(names1);
-        System.out.println(names1);
-        System.out.println("使用 Java 8 语法: ");
-          
-        tester.sortUsingJava8(names2);
-        System.out.println(names2);
-     }
-     
-     // 使用 java 7 排序
-     private void sortUsingJava7(List<String> names){   
-        Collections.sort(names, new Comparator<String>() {
-           @Override
-           public int compare(String s1, String s2) {
-              return s1.compareTo(s2);
-           }
-        });
-     }
-     
-     // 使用 java 8 排序
-     private void sortUsingJava8(List<String> names){
-        Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
-     }
+   public static void main(String args[]){
+      Flowable.range(1, 10)
+      .flatMap(v ->
+          Flowable.just(v)
+            .subscribeOn(Schedulers.computation())
+            .map(w -> w * w)
+      )
+      .blockingSubscribe(System.out::println);   
+   }
 }
